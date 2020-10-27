044ae3ab54725652d1bce1e52f83fa04db398c48
Alexei <1996Leha@mail.ru>
2020-10-27 15:53:20 +0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const { parallel } = require('gulp');
+const gulp = require('gulp'),
+    browserSync = require('browser-sync'),
+    rename = require("gulp-rename"),
+    cleanCSS = require('gulp-clean-css'),
+    autoprefixer = require('gulp-autoprefixer'),
+    sass = require('gulp-sass');
+
+
+gulp.task('server', function () {
+    browserSync.init({
+        server: {
+            baseDir: "src"
+        }
+    });
+});
+
+gulp.task('styles', function () {
+    return gulp.src("src/scss/*.+(scss|sass)")
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({
+            prefix: "",
+            suffix: ".min",
+        }))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function(){
+    gulp.watch("src/scss/*/*.+(scss|sass)", gulp.parallel("styles"))
+    gulp.watch("src/*.html").on("change", browserSync.reload);
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles'))
