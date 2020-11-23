e516915f25a79a4b03804bdf7d3ac8a1df0ce1d0
Alexei <1996Leha@mail.ru>
2020-11-24 03:07:30 +0500

    create media file

--- src/js/_script.js
+++ src/js/_script.js
@@ -2,11 +2,11 @@ $(function(){
     $('.carousel__inner').slick({
         speed : 1000,
         adaptiveHeight: true,
-        prevArrow : '<button type="button" class="slick-prev"><img src="../icons/arrows/prev.png"></button>',
-        nextArrow : '<button type="button" class="slick-next"><img src="../icons/arrows/next.png"></button>',
+        prevArrow : '<button type="button" class="slick-prev"><img src="icons/arrows/prev.png"></button>',
+        nextArrow : '<button type="button" class="slick-next"><img src="icons/arrows/next.png"></button>',
         responsive: [
             {
-              breakpoint: 992,
+              breakpoint: 993,
               settings: {
                   dots: true,
                   arrows: false
