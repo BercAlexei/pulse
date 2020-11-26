484f75f281c78eaa3bbce4b647f4f9331e6e57aa
Alexei <1996Leha@mail.ru>
2020-11-27 03:40:29 +0500

    add catalog

--- src/js/_script.js
+++ src/js/_script.js
@@ -1,4 +1,5 @@
 $(function(){
+  // add slider
     $('.carousel__inner').slick({
         speed : 1000,
         adaptiveHeight: true,
@@ -9,9 +10,32 @@ $(function(){
               breakpoint: 993,
               settings: {
                   dots: true,
-                  arrows: false
+                  arrows: false,
+                  adaptiveHeight: false
               }
             }
           ]
     });
+
+    // add tabs
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    // add link to tab
+    
+
+    function  toggleSlide(item) {
+      $(item).each(function(i){
+        $(this).on('click', function(e){
+          e.preventDefault();
+          $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active')
+          $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active')
+        })
+      });
+    };
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
   });
