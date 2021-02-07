bd510d1306bafb1e0d7566ae390c38ecec59adad
Alexei <1996Leha@mail.ru>
2021-02-07 18:33:32 +0500

    add validation forms

--- src/js/_script.js
+++ src/js/_script.js
@@ -43,15 +43,45 @@ $(function () {
   $('.modal__close').on('click', function () {
     $('.overlay, #consultation, #order, #thamks').fadeOut('slow');
   });
-  $('.button_mini').on('click', function(){
+  $('.button_mini').on('click', function () {
     $('.overlay, #order').fadeIn('slow');
   });
-  $('.button_mini').each(function(i){
-    $(this).on('click', function(){
+  $('.button_mini').each(function (i) {
+    $(this).on('click', function () {
       $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
     });
   });
 
-  $('.feed-form').validate();
+  // validation
+function validateForms(form){
+  $(form).validate({
+    rules: {
+      name: {
+        required: true,
+        minlength: 2
+      },
+      phone: "required",
+      email: {
+        required: true,
+        email: true
+      }
+    },
+    messages: {
+      name: {
+        required: "Пожалуйста, введите свое имя!",
+        minlength: jQuery.validator.format("Введите {0} символов!")
+      },
+      phone: "Пожалуйста, введите свой номер телефона",
+      email: {
+        required: "Пожалуйста, введите свою почту!",
+        email: "Неправильно введен адрес!"
+      }
+    }
+  });
+}
+
+validateForms ('#consultation-main');
+validateForms ('#consultation form');
+validateForms ('#order form');
 
 });
